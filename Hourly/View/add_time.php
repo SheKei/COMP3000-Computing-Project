d081f873b87bbff1e98404e068b69c8eac734901
SheKei <43881082+SheKei@users.noreply.github.com>
2020-11-11 13:34:14 +0000

    Pop-up page mechanic added for time log

---
+++ Hourly/View/add_time.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+
+<div class="modal fade" id="timeModal">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h4 class="modal-title">Add Time</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <div class="modal-body">
+                <div class="container" id="timeContainer">
+
+                    <form>
+
+                        <input type="submit" class="btn btn-primary" name="addTimeBtn" id="addTimeBtn" value="Add Time">
+                    </form>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
+

052413eeca1a608f6fe581e5c2859742d532d8db
SheKei <43881082+SheKei@users.noreply.github.com>
2020-11-13 12:39:47 +0000

    Fixed issue where user was not redirected back to page

--- Hourly/Controller/edit_module.php
+++ Hourly/Controller/edit_module.php
@@ -4,5 +4,5 @@ include_once 'module_controller.php';
 if(isset($_POST['saveModuleBtn'])) {
     $controller = new module_controller('dummy');
     $controller->updateModuleDetails($_POST['code'], $_POST['name'], $_POST['theColour'], $_POST['hour'], $_POST['moduleCodeCurrent']);
-    header('Location: ../View/module.php?code='.$_POST['moduleCode']);
+    header('Location: ../View/module.php?code='.$_POST['code']);
 }

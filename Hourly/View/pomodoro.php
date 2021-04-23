059eae15b89f643b787600ee3635c30a779cc7fd
SheKei <43881082+SheKei@users.noreply.github.com>
2021-04-23 11:01:42 +0100

    Fixed pomodoro issues
    Timer not working in time
    Selected time not chosen
    Disable/Enable functionality not working
    Unable to log time

--- Hourly/View/pomodoro.php
+++ Hourly/View/pomodoro.php
@@ -1,12 +1,17 @@
 <?php
-include_once '../Controller/Task_Controller.php';
-$taskController = new Task_Controller('dummy');
+//include_once '../Controller/Task_Controller.php';
+//$taskController = new Task_Controller('dummy');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/20c7401107.js" crossorigin="anonymous"></script>
     <title>Pomodoro Timer</title>
 
     <style>
@@ -23,15 +28,19 @@ $taskController = new Task_Controller('dummy');
 
         #inputPanel{
             width: 95%;
-            height: 600px;
+            min-height: 600px;
             background-color: white;
         }
 
         #timePanel{
-            width: 400px;
-            height: 200px;
+            width: 600px;
+            height: 300px;
             margin-right: auto;
             margin-left: auto;
+        }
+
+        #addTimePanel{
+
         }
 
         .text-center{
@@ -51,6 +60,21 @@ $taskController = new Task_Controller('dummy');
              font-size: 50px;
          }
 
+         #finishedMsg{
+             letter-spacing: 3px;
+             font-weight: bold;
+             font-size: 30px;
+             color: mediumseagreen;
+         }
+
+         #startBtn, #pauseBtn, #resetBtn{
+             font-size: 20px;
+         }
+
+         .hidden{
+             display: none;
+         }
+
     </style>
 
 </head>
@@ -66,7 +90,7 @@ include_once "../Public/side_navbar.php";
     <div id="inputPanel">
         <h2 class="text-center">Pomodoro</h2>
 
-
+        <form method="POST" action="../Controller/timeController.php">
         <div class="form-group row" id="taskSelection">
             <label for="taskName" class="col-form-label">Task: <label>
                     <div class="col-auto">
@@ -82,9 +106,9 @@ include_once "../Public/side_navbar.php";
         <div id="buttons" class="text-center">
 
             <p>
-                <button class="btn btn-dark time" id="decrement"><i class="fas fa-minus-circle"></i></button>
+                <button type="button" class="btn btn-dark time" id="decrementBtn"><i class="fas fa-minus-circle"></i></button>
                 <span id="timeChosen">25</span><span> minutes </span>
-                <button class="btn btn-dark time" id="increment"><i class="fas fa-plus-circle"></i></button>
+                <button type="button" class="btn btn-dark time" id="incrementBtn"><i class="fas fa-plus-circle"></i></button>
             </p>
 
         </div>
@@ -92,12 +116,28 @@ include_once "../Public/side_navbar.php";
         <div id="timePanel">
             <p class="text-center" id="timer"></p>
             <div id="btnPanel" class="text-center">
-                <button class="btn btn-success" id="button">Start</button>
-                <button class="btn btn-dark" id="reset">Reset</button>
-            </div>
-
+                <button type="button" class="btn btn-success" id="startBtn">Start</button>
+                <button type="button" class="btn btn-danger" id="pauseBtn">Pause</button>
+                <button type="button" class="btn btn-dark" id="resetBtn">Reset</button>
+                <button type="button" class="btn btn-default" id="testBtn">Test</button>
+            </div><br>
+            <p class="text-center hidden" id="finishedMsg">POMODORO SESSION COMPLETED - GOOD WORK!</p>
         </div>
+        <br>
+        <div id="addTimePanel">
+            <div class="row">
+                <div class="col-lg-9">
 
+                    <input type="number" class="form-control hidden" name="workedFor" id="workedFor" readonly>
+                    <textarea class="form-control text-center" id="description" name="description"
+                              placeholder="Add a description of what you did if you like..." cols="20" rows="5"></textarea>
+                </div>
+                <div class="col-lg-3 text-center">
+                    <br><br>
+                    <button type="submit" class="btn btn-success btn-lg" name="saveTimeBtn" id="saveTimeBtn">Add Time To Task</button>
+                    </form>
+                </div>
+        </div>
 
 
     </div>
@@ -111,59 +151,112 @@ include_once "../Public/side_navbar.php";
     $(function(){
         var totalMinutes = 25;
         var remainingTime = totalMinutes * 60;
+        var chosenTime;
+        var theTimer;
         $("#timer").html(calculateTime(remainingTime));
+        $("#saveTimeBtn").prop('disabled', true);
+        $("#workedFor").val(totalMinutes);
 
-        var onPause = true;
+        //Update the selected time for timer
+        function changeMinutes(){
+            totalMinutes = chosenTime;
+            remainingTime = totalMinutes * 60;
+            $("#workedFor").val(totalMinutes);
+        }
 
 
         //Increment time by 5
-        $("#increment").click(function(){
-            remainingTime = Number($("#timeChosen").html()); //Get current minutes
-            remainingTime = remainingTime + 5; //Increment current minutes by 5
-            displayTime(); //Display change in time
+        $("#incrementBtn").click(function(){
+            chosenTime = Number($("#timeChosen").html());   //Get current minutes
+            chosenTime = chosenTime + 5;                    //Increment current minutes by 5
+            displaySelectedTime();                          //Display change in time
+            changeMinutes();                                //Copy new time to timer
         });
 
         //Check and decrement current time by 5
-        $("#decrement").click(function(){
-            remainingTime = Number($("#timeChosen").html());
-            if(remainingTime > 5){
-                remainingTime = remainingTime - 5; //Decrement current if minutes more than 5
+        $("#decrementBtn").click(function(){
+            chosenTime = Number($("#timeChosen").html());   //Get current chosen time
+            if(chosenTime > 5){
+                chosenTime = chosenTime - 5;                //Decrement current if minutes more than 5
             }else{
-                remainingTime = 5;
+                chosenTime = 5;                             //Else leave at 5 minutes
             }
-            displayTime(); //Display change in time
+            displaySelectedTime();                          //Display change in time
+            changeMinutes();                                //Copy to new time to timer
         });
 
-        //When pomodoro start/pause btn is pressed
-        $("#button").click(function(){
 
-            if($("#button").hasClass("btn-success")){
-                changeToPauseBtn();
-            }else{
-                changeToStartBtn();
-            }
+        //Start btn to start timer
+        $("#startBtn").click(function(){
+           theTimer = setInterval(startTimer, 1000);        //Start counting down seconds
+           $("#startBtn").prop('disabled', true);           //Disable start btn
+           $("#pauseBtn").prop('disabled', false);          //Enable pause btn
+           $("#incrementBtn").prop('disabled', true);       //Disable option to change chosen time once timer enabled
+           $("#decrementBtn").prop('disabled', true);
+        });
+
+        //Pause btn to stop timer
+        $("#pauseBtn").click(function(){
+           clearInterval(theTimer);                         //Pause count down
+           $("#startBtn").prop('disabled', false);          //Enable start btn
+           $("#pauseBtn").prop('disabled', true);           //Disable pause btn
         });
 
         //Reset timer back to 25 minutes
-        $("#reset").click(function(){
-            $("#timer").html(calculateTime(25*60));
-            $("#timeChosen").html(25);
+        $("#resetBtn").click(function(){
+            clearInterval(theTimer);                        //Stop count down
+            resetTimer();                                   //Reset timer back to 25:00
+            $("#startBtn").prop('disabled', false);         //Enable start btn
+            $("#pauseBtn").prop('disabled', false);         //Enable pause btn
+            $("#incrementBtn").prop('disabled', false);     //Re-enable option to change chosen time once reset
+            $("#decrementBtn").prop('disabled', false);
+            $("#saveTimeBtn").prop('disabled', true);       //Disable btn to save time to task
         });
 
-        function startTimer(remainingTime){
+        //Reset timer back to 25 minutes
+        $("#testBtn").click(function(){
+            testTime();
+        });
 
+        //Deduct 1 second off remaining time
+        function startTimer(){
+            remainingTime--;
+            displayRemainingTime(remainingTime);
+            //IF TIMER IS FINISHED
+            if(remainingTime <= 0){
+                clearInterval(theTimer);
+                $("#startBtn").prop('disabled', true);      //Disable start btn
+                $("#pauseBtn").prop('disabled', true);      //Disable pause btn
+                $("#finishedMsg").removeClass("hidden");    //Reveal finished message
+                $("#saveTimeBtn").prop('disabled', false);    //Enable the add time btn
+            }
         }
 
-        function displayTime(){
-            //Display mm:ss in timer
-            $("#timer").html(calculateTime(remainingTime*60));
-            //Display current chosen time
-            $("#timeChosen").html(remainingTime);
+        //Display remaining time mm:ss during countdown
+        function displayRemainingTime(){
+            $("#timer").html(calculateTime(remainingTime));
+        }
+
+        //Display current chosen time on timer
+        function displaySelectedTime(){
+            $("#timeChosen").html(chosenTime);
+            $("#timer").html(calculateTime(chosenTime*60));
+        }
+
+        //Reset remaining time back to 25 minutes
+        function resetTimer(){
+            totalMinutes = 25;
+            remainingTime = totalMinutes * 60;
+            displayRemainingTime();
+        }
+
+        function testTime(){
+            remainingTime = 5;
         }
 
     });
 
-    //Format time into mm:ss
+    //Format time in seconds into mm:ss
     function calculateTime(time){
         let quotient = parseInt(time/60);
         let remainder = time%60;
@@ -174,15 +267,4 @@ include_once "../Public/side_navbar.php";
         return quotient + ":" + remainder;
     }
 
-    function changeToStartBtn(){
-        $("#button").removeClass("btn-danger");
-        $("#button").addClass("btn-success");
-        $("#button").html("Start");
-    }
-
-    function changeToPauseBtn(){
-        $("#button").removeClass("btn-success");
-        $("#button").addClass("btn-danger");
-        $("#button").html("Pause");
-    }
 </script>

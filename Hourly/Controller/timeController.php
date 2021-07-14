07ca6a049fc4cf7aff9a74f58044478ec3818079
SheKei <43881082+SheKei@users.noreply.github.com>
2021-07-14 10:30:33 +0100

    Update timeController.php

--- Hourly/Controller/timeController.php
+++ Hourly/Controller/timeController.php
@@ -5,17 +5,7 @@ $controller = new Time_Controller('dummy');
 //POST request add time to a task
 if(isset($_POST['addTimeBtn'])){
     $duration = $_POST['hour'].":".$_POST['minute'];
-    $timeStamp = "";
-
-    //Check if user log is for today or another date
-    if(isset($_POST['todayDate'])){
-        $timeStamp = date("Y-m-d");
-    }else{
-        $timeStamp = $_POST['date'];
-    }
-
-    $controller->add_time($_POST['taskName'], $duration, $_POST['description'], $timeStamp);
-
+    $controller->add_time($_POST['taskName'], $duration, $_POST['description'], $_POST['date']);
     header('Location: ../View/home.php');
 }
 
